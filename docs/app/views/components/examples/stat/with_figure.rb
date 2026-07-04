86e055f5b3eea11e020a48bbc78190102490a618
Mikael Henriksson <mikael@mhenrixon.com>
2026-07-04 12:26:03 +0200

    chore(docs): docs-kit 1.0.4 + rouge 5 + dependency refresh (#34)

--- docs/app/views/components/examples/stat/with_figure.rb
+++ docs/app/views/components/examples/stat/with_figure.rb
@@ -32,18 +32,18 @@ module Views
           def heart_icon
             svg(xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24",
                 class: "inline-block h-8 w-8 stroke-current") do |s|
-                  s.path(stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "2",
-                         d: "M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 " \
-                            "00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z")
-                end
+              s.path(stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "2",
+                     d: "M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 " \
+                        "00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z")
+            end
           end
 
           def bolt_icon
             svg(xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24",
                 class: "inline-block h-8 w-8 stroke-current") do |s|
-                  s.path(stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "2",
-                         d: "M13 10V3L4 14h7v7l9-11h-7z")
-                end
+              s.path(stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "2",
+                     d: "M13 10V3L4 14h7v7l9-11h-7z")
+            end
           end
         end
       end
