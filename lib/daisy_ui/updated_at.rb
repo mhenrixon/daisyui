d1bbde1e58a4f7827d6c9205dc1f70d9beccda8c
mhenrixon <mikael@mhenrixon.com>
2026-08-31 15:48:11 +0200

    chore: bump version to 1.3.1

--- lib/daisy_ui/updated_at.rb
+++ lib/daisy_ui/updated_at.rb
@@ -3,5 +3,5 @@
 module DaisyUI
   # This timestamp is automatically updated when releasing a new version
   # Format: YYYY-MM-DD HH:MM:SS UTC
-  UPDATED_AT = "2026-08-30 12:32:05 UTC"
+  UPDATED_AT = "2026-08-31 13:48:07 UTC"
 end
